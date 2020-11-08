1e801f0f5c6a2a0b531407440c62bd6f4fe57eee
Ori Shuchami <ori_s@ifat.com>
2020-11-08 12:37:34 +0200

    changed dateCombo to react-select

--- src/common/components/SearchInput/DateCombo/index.js
+++ src/common/components/SearchInput/DateCombo/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
+import { observer } from 'mobx-react'
+import { observable } from 'mobx'
+import Select from 'react-select'
 import './DateCombo.scss'
-//import {Select} from 'antd'
 
+/*
 const DateCombo = ({chooseDateField, t}) => {
-  /*const { Option } = Select
-  return <Select defaultValue="inputDate" bordered={false} >
-    <Option value="inputDate" onClick={() => chooseDateField('inputDate')}>{t('filter.inputDate')}</Option>
-    <Option value="presentationDate" onClick={() => chooseDateField('presentationDate')}>{t('filter.presentationDate')}</Option>
-    <Option value="resultDate" onClick={() => chooseDateField('resultDate')}>{t('filter.resultDate')}</Option>
-  </Select>*/
+  
   return <ul className="dropdown menu align-left sort" styleName="sort" id="dateCombo" data-dropdown-menu data-disable-hover="true" data-click-open="true">
     <li>      
       <ul className="menu">                 
@@ -18,6 +16,48 @@ const DateCombo = ({chooseDateField, t}) => {
       </ul>
     </li>
   </ul>
-}
+}*/
+export default @observer class DateCombo extends React.Component {
 
-export default DateCombo
+  @observable value = {key: 'תאריך פרסום', value: 'inputDate'}
+
+  onChange = values => {
+    console.log(values);
+    this.value = values
+    const {chooseDateField} = this.props
+    chooseDateField(values.value)
+  }
+
+  onInputKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      //e.preventDefault()  //fucks up the search.
+      e.stopPropagation()
+    }
+  }
+
+  render() {
+    const {t} = this.props
+    const options = [{key: t('filter.inputDate'), value: 'inputDate' },
+      {key: t('filter.presentationDate'), value: 'presentationDate' },
+      {key: t('filter.resultDate'), value: 'resultDate' }]
+    return  <Select
+      styleName="branch"
+      className="single-select"
+      name="searchbox"
+      placeholder={t('agent.placeHolder')}
+      noResultsText={null}
+      searchPromptText=""
+      rtl={true}
+      multi={false}
+      cache={false}
+      clearable={false}
+      searchable={false}
+      options={options}
+      onChange={this.onChange}
+      onInputKeyDown={this.onInputKeyDown}
+      value={this.value}
+      labelKey={'key'}
+      valueKey={'value'}
+    />
+  }
+}
