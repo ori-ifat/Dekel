d99fd8b154e38350ad4c40651680e21391db42f3
Ori Shuchami <ori_s@ifat.com>
2020-11-08 14:02:41 +0200

    changed back to simple <ul> tag

--- src/common/components/SearchInput/DateCombo/index.js
+++ src/common/components/SearchInput/DateCombo/index.js
@@ -1,63 +1,36 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import { observable } from 'mobx'
-import Select from 'react-select'
+import enhanceWithClickOutside from 'react-click-outside'
 import './DateCombo.scss'
 
-/*
-const DateCombo = ({chooseDateField, t}) => {
+export default
+@enhanceWithClickOutside 
+@observer 
+class DateCombo extends React.Component {
   
-  return <ul className="dropdown menu align-left sort" styleName="sort" id="dateCombo" data-dropdown-menu data-disable-hover="true" data-click-open="true">
-    <li>      
-      <ul className="menu">                 
-        <li><a onClick={() => chooseDateField('inputDate')}>{t('filter.inputDate')}</a></li>
-        <li><a onClick={() => chooseDateField('presentationDate')}>{t('filter.presentationDate')}</a></li>
-        <li><a onClick={() => chooseDateField('resultDate')}>{t('filter.resultDate')}</a></li>
-      </ul>
-    </li>
-  </ul>
-}*/
-export default @observer class DateCombo extends React.Component {
+  @observable shown = false
 
-  @observable value = {key: 'תאריך פרסום', value: 'inputDate'}
-
-  onChange = values => {
-    console.log(values);
-    this.value = values
-    const {chooseDateField} = this.props
-    chooseDateField(values.value)
+  setShown = () => {
+    this.shown = !this.shown
   }
 
-  onInputKeyDown = (e) => {
-    if (e.keyCode === 13) {
-      //e.preventDefault()  //fucks up the search.
-      e.stopPropagation()
-    }
+  handleClickOutside() {
+    //console.log('handleClickOutside')
+    this.shown = false
   }
 
   render() {
-    const {t} = this.props
-    const options = [{key: t('filter.inputDate'), value: 'inputDate' },
-      {key: t('filter.presentationDate'), value: 'presentationDate' },
-      {key: t('filter.resultDate'), value: 'resultDate' }]
-    return  <Select
-      styleName="branch"
-      className="single-select"
-      name="searchbox"
-      placeholder={t('agent.placeHolder')}
-      noResultsText={null}
-      searchPromptText=""
-      rtl={true}
-      multi={false}
-      cache={false}
-      clearable={false}
-      searchable={false}
-      options={options}
-      onChange={this.onChange}
-      onInputKeyDown={this.onInputKeyDown}
-      value={this.value}
-      labelKey={'key'}
-      valueKey={'value'}
-    />
+    const {chooseDateField, t} = this.props
+    return <ul styleName="combo-wrapper">
+      <li styleName="combo-container">
+        <a onClick={this.setShown}>show</a>  
+        <ul style={this.shown ? null : {display: 'none'}} styleName="combo">                 
+          <li styleName="combo-item"><a onClick={() => chooseDateField('inputDate')}>{t('filter.inputDate')}</a></li>
+          <li styleName="combo-item"><a onClick={() => chooseDateField('presentationDate')}>{t('filter.presentationDate')}</a></li>
+          <li styleName="combo-item"><a onClick={() => chooseDateField('resultDate')}>{t('filter.resultDate')}</a></li>
+        </ul>
+      </li>
+    </ul>
   }
 }
